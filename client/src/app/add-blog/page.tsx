4b61c8864662845a196899472c2487967eb7309a
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-22 18:00:49 +0400

    now we can add image with the image adress

--- client/src/app/add-blog/page.tsx
+++ client/src/app/add-blog/page.tsx
@@ -10,12 +10,26 @@ const AddProductPage = () => {
   const [description, setDescription] = useState<string>('')
   const [finalImage, setFinalImage] = useState<string>('')
 
+  const addImage = () => {
+    if (imageAddress.length === 0) {
+      return alert('image must not be empty')
+    }
+    setFinalImage(imageAddress)
+    setImageAddress('')
+  }
+
   return (
     <main className='w-full min-h-[100vh] bg-bgColor text-white'>
       <form className='pt-32 px-10 flex flex-col gap-6 items-start mx-auto w-[800px]'>
         <h2 className='text-2xl font-medium mb-12'>Add your blog</h2>
         <div className='w-full flex flex-col items-start gap-8 mb-6'>
-          <div className='w-1/2 h-[300px] border rounded-xl flex items-center justify-center'></div>
+          <div className='w-1/2 h-[300px] overflow-hidden border rounded-xl flex items-center justify-center'>
+            {finalImage ? (
+              <img src={finalImage} className='w-full h-full object-cover' />
+            ) : (
+              'Your Image'
+            )}
+          </div>
           <input
             value={imageAddress}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -26,6 +40,7 @@ const AddProductPage = () => {
             className='w-full bg-bgColor border border-white rounded py-2 px-5 placeholder:text-white'
           />
           <button
+            onClick={addImage}
             type='button'
             className='bg-white text-black px-5 py-1 rounded hover:opacity-80 transition-all duration-300 ease-linear'
           >

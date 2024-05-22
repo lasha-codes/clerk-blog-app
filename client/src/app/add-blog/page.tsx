f80d05bc8d205b3e3cfa48c2a6124f49d64a5670
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-05-22 18:11:58 +0400

    adding types is done looks great

--- client/src/app/add-blog/page.tsx
+++ client/src/app/add-blog/page.tsx
@@ -9,6 +9,7 @@ const AddProductPage = () => {
   const [title, setTitle] = useState<string>('')
   const [description, setDescription] = useState<string>('')
   const [finalImage, setFinalImage] = useState<string>('')
+  const [selectedTypes, setSelectedTypes] = useState<string[]>([])
 
   const addImage = () => {
     if (imageAddress.length === 0) {
@@ -18,8 +19,29 @@ const AddProductPage = () => {
     setImageAddress('')
   }
 
+  const toggleType = (type: string) => {
+    let selectedTypesCopy = [...selectedTypes]
+    if (selectedTypes.includes(type)) {
+      selectedTypesCopy = selectedTypesCopy.filter((selectedType: string) => {
+        return selectedType !== type
+      })
+    } else {
+      selectedTypesCopy = [...selectedTypes, type]
+    }
+    setSelectedTypes(selectedTypesCopy)
+  }
+
+  const types = [
+    'GLOBAL',
+    'SPORTS',
+    'NATURE',
+    'BEAUTY',
+    'ADVERTISEMENT',
+    'POLITICS',
+  ]
+
   return (
-    <main className='w-full min-h-[100vh] bg-bgColor text-white'>
+    <main className='w-full min-h-[100vh] py-12 bg-bgColor text-white'>
       <form className='pt-32 px-10 flex flex-col gap-6 items-start mx-auto w-[800px]'>
         <h2 className='text-2xl font-medium mb-12'>Add your blog</h2>
         <div className='w-full flex flex-col items-start gap-8 mb-6'>
@@ -75,6 +97,38 @@ const AddProductPage = () => {
             id='description'
             className='bg-bgColor border-white placeholder:text-white border rounded outline-none px-5 placeholder:opacity-90 py-1.5 w-full'
           />
+        </div>
+        <div className='w-full'>
+          <div className='flex flex-col items-start gap-2 w-full'>
+            <h2 className='text-lg font-medium'>Select types</h2>
+            <div className='w-[710px] flex items-center gap-3 flex-wrap'>
+              {types.map((type: string, idx: number) => {
+                return (
+                  <button
+                    onClick={() => toggleType(type)}
+                    type='button'
+                    key={idx}
+                    className='text-black bg-white rounded-full px-4 py-1'
+                  >
+                    {type}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
+          <div className='w-[710px] border-b border-white py-5 flex items-center gap-4 flex-wrap'>
+            {selectedTypes.map((type: string, idx: number) => {
+              return (
+                <button
+                  type='button'
+                  key={idx}
+                  className='bg-white text-black px-4 py-1 rounded-full'
+                >
+                  {type}
+                </button>
+              )
+            })}
+          </div>
         </div>
       </form>
     </main>
